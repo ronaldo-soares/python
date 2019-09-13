d6cea6fe5bc9e9ebad00748d3f6bd467c796223e
rsoares <rsoares@uolinc.com>
2019-09-13 20:09:54 -0300

    testing...

--- argtest.py
+++ argtest.py
@@ -1,7 +1,8 @@
 import argparse
 import os.path
+import sys
 
-# ...
+
 parser = argparse.ArgumentParser(
     usage="usage aqui",
     description="description aqui"
@@ -20,7 +21,8 @@ parser.add_argument(
     "--file",
     type=str,
     dest="nam_file",
-    help="-f --file fileName format csv"
+    help="-f --file fileName format csv",
+    required=True
 )
 parser.add_argument(
     "-w",
@@ -31,9 +33,36 @@ parser.add_argument(
     default=1
 )
 
-
 args = parser.parse_args()
 
+if not os.path.isfile(args.nam_file):
+    print 'Err: file %s not found' % args.nam_file
+    sys.exit(1)
 
-if args.nam_worker:
-    print args.nam_worker
+f = open(args.nam_file)
+f1 = f.readlines()
+for x in f1:
+    print 'bucetada'
+    print x #x.replace('\n','')
+    print 'ota buceta'
+#urls = []
+#with open(args.nam_file, 'r') as file:
+#    #print file.read()
+#    urls.append(file.read())
+
+#print urls
+
+
+'''
+try:
+    response = requests.post('http://localhost:808/index_example2.html', headers=headers, data=data)
+except requests.exceptions.Timeout:
+    print "Maybe set up for a retry, or continue in a retry loop"
+except requests.exceptions.TooManyRedirects:
+    print "Tell the user their URL was bad and try a different one"
+except requests.exceptions.RequestException as e:
+    print "catastrophic error. bail."
+    print e
+'''
+
+
